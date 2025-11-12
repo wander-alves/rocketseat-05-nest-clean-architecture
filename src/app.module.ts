a21cd475e103752fe77090c42e26133ddf7ae805
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-11-12 10:55:10 -0300

    feat: adds create-account-controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAccountController } from 'src/controllers/create-account.controller';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body;
+
+    const alreadyRegisteredAccount = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (alreadyRegisteredAccount) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      );
+    }
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+  }
+}
