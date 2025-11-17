bd459f2afec0e5ece8ceaf260e74098039bf29c7
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-11-17 20:53:54 -0300

    feat: implementing auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAccountController } from 'src/controllers/create-account.controller';
 import { envSchema } from 'src/env';
 import { AuthModule } from 'src/auth/auth.module';
+import { AuthenticateController } from 'src/controllers/authenticate.controller';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { AuthModule } from 'src/auth/auth.module';
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
