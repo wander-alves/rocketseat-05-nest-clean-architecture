8f51ce45eb5613e584ac6098b818676f67bf598b
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-01-18 21:04:00 -0300

    feat: add cache test

---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.e2e-spec.ts
@@ -0,0 +1,122 @@
+import { describe, beforeAll, it, expect } from 'vitest';
+
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { StudentFactory } from 'tests/factories/make-student';
+import { QuestionFactory } from 'tests/factories/make-question';
+import { AttachmentFactory } from 'tests/factories/make-attachment';
+import { QuestionAttachmentFactory } from 'tests/factories/make-question-attachment';
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
+import { CacheRepository } from '@/infra/cache/cache-repository';
+import { CacheModule } from '@/infra/cache/cache.module';
+
+describe('[E2E] Prisma Questions Repository', () => {
+  let app: INestApplication;
+  let questionsRepository: QuestionsRepository;
+  let cacheRepository: CacheRepository;
+  let studentFactory: StudentFactory;
+  let attachmentFactory: AttachmentFactory;
+  let questionAttachmentFactory: QuestionAttachmentFactory;
+  let questionFactory: QuestionFactory;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule, CacheModule],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    cacheRepository = moduleRef.get(CacheRepository);
+    questionsRepository = moduleRef.get(QuestionsRepository);
+    studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory);
+
+    await app.init();
+  });
+
+  it('should cache question details', async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const attachment = await attachmentFactory.makePrismaAttachment();
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id.toString(),
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      questionId: question.id.toString(),
+      attachmentId: attachment.id.toString(),
+    });
+
+    const slug = question.slug.value;
+    const questionDetails = await questionsRepository.findDetailsBySlug(slug);
+
+    const cached = await cacheRepository.get(`question:${slug}:details`);
+
+    expect(cached).toEqual(JSON.stringify(questionDetails));
+  });
+
+  it('should return cached question details on subsequent calls', async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const attachment = await attachmentFactory.makePrismaAttachment();
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id.toString(),
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      questionId: question.id.toString(),
+      attachmentId: attachment.id.toString(),
+    });
+
+    const slug = question.slug.value;
+
+    await cacheRepository.set(
+      `question:${slug}:details`,
+      JSON.stringify({ test: true }),
+    );
+
+    const questionDetails = await questionsRepository.findDetailsBySlug(slug);
+
+    expect(questionDetails).toEqual({ test: true });
+  });
+
+  it('should reset question details cache when saving the question', async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const attachment = await attachmentFactory.makePrismaAttachment();
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id.toString(),
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      questionId: question.id.toString(),
+      attachmentId: attachment.id.toString(),
+    });
+
+    const slug = question.slug.value;
+
+    await cacheRepository.set(
+      `question:${slug}:details`,
+      JSON.stringify({ test: true }),
+    );
+
+    await questionsRepository.save(question);
+
+    const cached = await cacheRepository.get(slug);
+
+    expect(cached).toBeNull();
+  });
+});
--- tests/setup-e2e.ts
+++ tests/setup-e2e.ts
@@ -4,17 +4,26 @@ import { config } from 'dotenv';
 import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
 import { DomainEvents } from '@/core/events/domain-events';
+import Redis from 'ioredis';
+import { envSchema } from '@/infra/env/env';
 
 config({ path: '.env', override: true });
 config({ path: '.env.test', override: true });
 
+const env = envSchema.parse(process.env);
+
 const prismaClient = new PrismaClient();
+const redisClient = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+});
 
 function generateDatabaseURL(schemaID: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Missing environment variable DATABASE_URL');
   }
-  const databaseURL = process.env.DATABASE_URL;
+  const databaseURL = env.DATABASE_URL;
   const url = new URL(databaseURL);
 
   url.searchParams.set('schema', schemaID);
@@ -23,11 +32,13 @@ function generateDatabaseURL(schemaID: string) {
 }
 
 const schemaID = randomUUID();
-beforeAll(() => {
+beforeAll(async () => {
   const randomDatabaseURL = generateDatabaseURL(schemaID);
   process.env.DATABASE_URL = randomDatabaseURL;
 
   DomainEvents.shouldRun = false;
+
+  await redisClient.flushdb();
 
   execSync('npx prisma migrate deploy');
 });
